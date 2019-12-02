75032b13fb223a9705c95ff15e42f2274a8b37c5
nate4101 <50373738+nate4101@users.noreply.github.com>
2019-12-02 16:44:43 -0500

    dfgdg

--- 5-0Cab/WebContent/js/dispatch.js
+++ 5-0Cab/WebContent/js/dispatch.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {  // When HTML DOM "click" event is invoked on element with ID "somebutton", execute the following function...
     $.get("RetreiveRequests", function(responseJson) {    // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response JSON...
         var $ul = $("<ul>").appendTo($("#append")); // Create HTML <ul> element and append it to HTML DOM element with ID "somediv".
+       document.getElementById("f1").value=responseJson[0];
         $.each(responseJson, function(index, item) { // Iterate over the JSON array.
             $("<li>").text(responseJson[0]).appendTo($ul);      // Create HTML <li> element, set its text content with currently iterated item and append it to the <ul>.
         });
