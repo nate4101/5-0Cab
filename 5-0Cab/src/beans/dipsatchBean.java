5d1ce4c4e03d50a48796c248810326914422e418
nate4101 <50373738+nate4101@users.noreply.github.com>
2019-12-02 10:33:24 -0500

    bean for dispatcher page to use with ajax

--- 5-0Cab/src/beans/dipsatchBean.java
+++ 5-0Cab/src/beans/dipsatchBean.java
@@ -2,4 +2,56 @@ package beans;
 
 public class dipsatchBean {
 
+	private String ID, Time, Special, Location, number;
+	
+	public void setID(String i)
+	{
+		this.ID = i;
+	}
+	
+	public String getID()
+	{
+		return this.ID;
+	}
+	
+	public void setTime(String i)
+	{
+		this.ID = i;
+	}
+	
+	public String getTime()
+	{
+		return this.Time;
+	}
+	
+	public void setSpecial(String i)
+	{
+		this.Special = i;
+	}
+	
+	public String getSpecial()
+	{
+		return this.Special;
+	}
+	
+	public void setLocation(String i)
+	{
+		this.Location = i;
+	}
+	
+	public String getLocation()
+	{
+		return this.Location;
+	}
+	
+	public void setnumber(String i)
+	{
+		this.number = i;
+	}
+	
+	public String getnumber()
+	{
+		return this.ID;
+	}
+	
 }
