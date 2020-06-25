6fd7caecf40435ffb2b9259190343a1bd2e6dfe3
nate4101 <nate4101@gmail.com>
2020-06-25 18:49:02 -0400

    Something

--- 5-0Cab/src/helper/EnviromentVariables.java
+++ 5-0Cab/src/helper/EnviromentVariables.java
@@ -4,4 +4,6 @@ public class EnviromentVariables {
 
 	public static final String jdbcURL = "jdbc:mysql://fivezerocabdb.cq6c0sods0tf.ca-central-1.rds.amazonaws.com:3306/cab?user=nate4101&password=fivezerocab";
 	public static final int one = 1;
+	
+	
 }
