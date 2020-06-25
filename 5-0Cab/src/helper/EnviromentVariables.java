09592bebb53c9e3177d49f50c4ead1d6394632d1
nate4101 <nate4101@gmail.com>
2020-06-25 17:44:20 -0400

    Test gitignore

--- 5-0Cab/src/helper/EnviromentVariables.java
+++ 5-0Cab/src/helper/EnviromentVariables.java
@@ -3,4 +3,5 @@ package helper;
 public class EnviromentVariables {
 
 	public static final String jdbcURL = "jdbc:mysql://fivezerocabdb.cq6c0sods0tf.ca-central-1.rds.amazonaws.com:3306/cab?user=nate4101&password=fivezerocab";
+	public static final int one = 1;
 }
